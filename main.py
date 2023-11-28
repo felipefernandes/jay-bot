58a6c0f82f9315e3f90719ce4fd7d92312a80376
Felipe Fernandes <ffernandes@ffernandes-macbook_wifi.gazeus.int>
2023-11-28 14:43:58 -0300

    Organizacao de arquivos, separando modulo de configuracao

--- main.py
+++ main.py
@@ -1,24 +1,18 @@
 """
 Esse modulo configura e executa um bot SLack que interage com JIRA
 """
-
-import os
-from dotenv import load_dotenv
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from config import BOT_USER_OAUTH_TOKEN
 
-load_dotenv()
-
-JIRA_API_TOKEN = os.getenv("JIRA_API_TOKEN")
-SLACK_API_TOKEN = os.getenv("SLACK_TOKEN")
-BOT_USER_OAUTH_TOKEN = os.getenv("BOT_OAUTH_TOKEN")
 
 # Token do bot
 client = WebClient(token=BOT_USER_OAUTH_TOKEN)
 
 try:
-    #Enviar mensagem para um canal
-    response = client.chat_postMessage(channel='#transformacao-agil', text='Olár')
+    # Enviar mensagem para um canal
+    response = client.chat_postMessage(
+        channel='#transformacao-agil', text='Olár')
     assert response["message"]["text"] == "Olár"
 except SlackApiError as e:
     assert e.response["error"]
