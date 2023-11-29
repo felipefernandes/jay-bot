125502fc71ac5ac45708b9f040208562dbc2bc8a
Felipe Fernandes <felipefernandesweb@gmail.com>
2023-11-29 13:59:02 -0300

    Adicionando mensagens personalizadas aleatorias a resposta do bot

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 Esse módulo configura e executa um bot Slack que interage com JIRA.
 """
 import json
+import random
 from slack_sdk import WebClient
 from config import BOT_USER_OAUTH_TOKEN, JIRA_API_TOKEN, JIRA_SERVER_URL, JIRA_USER_EMAIL
 from jira_client import get_jira_client, check_wip_limit
@@ -17,6 +18,14 @@ def check_teams_wip():
         print("Arquivo de configuração não encontrado.")
         exit(1)
 
+    # Carregar mensagens de WIP
+    try:
+        with open('wip_messages.json', encoding='utf-8') as f:
+            wip_messages = json.load(f)["messages"]
+    except FileNotFoundError:
+        print("Arquivo de mensagens WIP não encontrado.")
+        exit(1)
+
     # Configurar clientes
     slack_client = WebClient(token=BOT_USER_OAUTH_TOKEN)
     jira_client = get_jira_client(
@@ -25,5 +34,7 @@ def check_teams_wip():
     # Verificar WIP para cada equipe e enviar alertas
     for team, config in teams_config.items():
         if check_wip_limit(jira_client, config['board_id'], config['wip_limit'], config['states']):
-            send_slack_message(
-                slack_client, config['channel'], f"Alerta de WIP excedido para {team}")
+            # Escolher uma mensagem aleatória
+            message = random.choice(wip_messages).format(
+                team=config['team_name'])
+            send_slack_message(slack_client, config['channel'], message)
