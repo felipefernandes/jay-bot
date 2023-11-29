9d1c87b281e34d64d87b3bbb43119b5cee87c061
Felipe Fernandes <ffernandes@sm-macbook-ffernandes.local>
2023-11-29 09:37:32 -0300

    implementacao parcial do webhook

---
+++ app.py
@@ -0,0 +1,20 @@
+"""
+Modulo Flask para tratar e processar os webhooks
+"""
+from flask import Flask, request
+from main import check_teams_wip
+
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    """
+    # Aqui você pode processar os dados recebidos do JIRA se necessário
+    """
+    check_teams_wip()  # Esta função deve ser ajustada no main.py
+    return "Webhook recebido", 200
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++ main.py
@@ -5,22 +5,28 @@ Esse módulo configura e executa um bot Slack que interage com JIRA.
 import json
 from slack_sdk import WebClient
 from config import BOT_USER_OAUTH_TOKEN, JIRA_API_TOKEN, JIRA_SERVER_URL, JIRA_USER_EMAIL
-from jira_client import get_jira_client, check_wip_limit
+from jira_client import get_jira_client
 from slack_client import send_slack_message
 
-# Carregar configurações das equipes
-try:
-    with open('teams_config.json', encoding='utf-8') as f:
-        teams_config = json.load(f)
-except FileNotFoundError:
-    print("Arquivo de configuração não encontrado.")
-    exit(1)
 
-# Configurar clientes
-slack_client = WebClient(token=BOT_USER_OAUTH_TOKEN)
-jira_client = get_jira_client(JIRA_API_TOKEN, JIRA_SERVER_URL, JIRA_USER_EMAIL)
+def check_teams_wip():
+    """
+    Carregar configurações das equipes
+    """
+    try:
+        with open('teams_config.json', encoding='utf-8') as f:
+            teams_config = json.load(f)
+    except FileNotFoundError:
+        print("Arquivo de configuração não encontrado.")
+        exit(1)
 
-# Verificar WIP para cada equipe e enviar alertas
-for team, config in teams_config.items():
-    if check_wip_limit(jira_client, config['board_id'], config['wip_limit'], config['states']):
-        send_slack_message(slack_client, config['channel'], f"Alerta de WIP excedido para {team}")
+    # Configurar clientes
+    slack_client = WebClient(token=BOT_USER_OAUTH_TOKEN)
+    jira_client = get_jira_client(
+        JIRA_API_TOKEN, JIRA_SERVER_URL, JIRA_USER_EMAIL)
+
+    # Verificar WIP para cada equipe e enviar alertas
+    for team, config in teams_config.items():
+        if check_wip_limit(jira_client, config['board_id'], config['wip_limit'], config['states']):
+            send_slack_message(
+                slack_client, config['channel'], f"Alerta de WIP excedido para {team}")
