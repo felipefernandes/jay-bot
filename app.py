3b9e779ea813389a318867db5057cccfb4da7d73
Felipe Fernandes <ffernandes@ffernandes-macbook_wifi.gazeus.int>
2023-11-30 13:59:56 -0300

    ajustando webhook headers format json

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ Modulo Flask para tratar e processar os webhooks
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from main import check_team_wip
 
 app = Flask(__name__)
@@ -12,15 +12,18 @@ app = Flask(__name__)
 
 @app.route('/wipcheck', methods=['POST'])
 def webhook():
-    # Extrair o team_id do cabeçalho da requisição
-    team_id = request.headers.get('team_id')
+    # Extrair dados JSON do corpo da requisição
+    data = request.get_json()
 
-    if not team_id:
-        return "Header 'team_id' não encontrado", 400
+    # Verificar se os dados JSON foram recebidos corretamente
+    if not data or 'team_id' not in data:
+        return jsonify({"error": "Dados JSON inválidos ou 'team_id' não encontrado"}), 400
+
+    team_id = data['team_id']
 
     # Chamar a função de verificação passando o team_id
     check_team_wip(team_id)
-    return "Webhook processado", 200
+    return jsonify({"message": "Webhook processado"}), 200
 
 
 if __name__ == "__main__":
