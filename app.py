ab2c5054a3ebdfe62e79ec0022f409c8f6c3bdeb
Felipe Fernandes <felipefernandesweb@gmail.com>
2024-05-24 17:32:03 -0300

    Adicionando novo endpoint sheet-update para notificacao

--- app.py
+++ app.py
@@ -5,13 +5,13 @@ Modulo Flask para tratar e processar os webhooks
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 from flask import Flask, request, jsonify
-from main import check_team_wip
+from main import check_team_wip, sheet_notification_update
 
 app = Flask(__name__)
 
 
 @app.route('/wipcheck', methods=['POST'])
-def webhook():
+def wip_check():
     # Extrair dados JSON do corpo da requisição
     data = request.get_json()
 
@@ -24,6 +24,24 @@ def webhook():
     # Chamar a função de verificação passando o team_id
     check_team_wip(team_id)
     return jsonify({"message": "Webhook processado"}), 200
+
+
+@app.route('/sheet-update', methods=['POST'])
+def sheet_update():
+    # Extrair dados JSON do corpo da requisicao
+    data = request.get_json()
+
+    # Verificacao se os dados JSON foram recebidos corretamente
+    if not data or 'team_id' not in data or 'sheet_name' not in data or 'num_rows_updated' not in data:
+        return jsonify({"error": "Dados JSON inválidos ou não encontrados"}), 400
+
+    team_id = data['team_id']
+    sheet_name = data['sheet_name']
+    num_rows_updated = data['num_rows_updated']
+
+    # Processando e enviando mensagens para o Slack
+    sheet_notification_update(
+        team_id, sheet_name, num_rows_updated)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,41 +1,44 @@
 """
 Esse módulo configura e executa um bot Slack que interage com JIRA.
 """
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
 import json
 import random
+import logging
 from slack_sdk import WebClient
 from config import BOT_USER_OAUTH_TOKEN, JIRA_API_TOKEN, JIRA_SERVER_URL, JIRA_USER_EMAIL
 from jira_client import get_jira_client, check_wip_limit
 from slack_client import send_slack_message
 
+# Configurar logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Inicializar o cliente Slack
+slack_client = WebClient(token=BOT_USER_OAUTH_TOKEN)
+
+
+def load_json_file(filename):
+    try:
+        with open(filename, encoding='utf-8') as f:
+            return json.load(f)
+    except FileNotFoundError:
+        logger.error(f"Arquivo de configuração não encontrado: {filename}")
+        exit(1)
+
 
 def check_team_wip(team_id):
     # Carregar configurações das equipes
-    try:
-        with open('teams_config.json', encoding='utf-8') as f:
-            teams_config = json.load(f)
-    except FileNotFoundError:
-        print("Arquivo de configuração não encontrado.")
-        exit(1)
+    teams_config = load_json_file('teams_config.json')
 
     # Carregar mensagens de WIP
-    try:
-        with open('wip_messages.json', encoding='utf-8') as f:
-            wip_messages = json.load(f)["messages"]
-    except FileNotFoundError:
-        print("Arquivo de mensagens WIP não encontrado.")
-        exit(1)
+    wip_messages = load_json_file('wip_messages.json').get("messages", [])
 
     team_config = teams_config.get(team_id)
     if not team_config:
-        print(f"Configuração não encontrada para o time: {team_id}")
+        logger.error(f"Configuração não encontrada para o time: {team_id}")
         return
 
-    # Configurar clientes
-    slack_client = WebClient(token=BOT_USER_OAUTH_TOKEN)
+    # Configurar cliente JIRA
     jira_client = get_jira_client(
         JIRA_API_TOKEN, JIRA_SERVER_URL, JIRA_USER_EMAIL)
 
@@ -47,5 +50,21 @@ def check_team_wip(team_id):
     # Verificar WIP para o time específico e enviar alerta
     if check_wip_limit(jira_client, board_id, wip_limit, states):
         # Escolher uma mensagem aleatória
-        message = random.choice(wip_messages).format(team=team_config['team_name'])
+        message = random.choice(wip_messages).format(
+            team=team_config['team_name'])
         send_slack_message(slack_client, team_config['channel'], message)
+
+
+def sheet_notification_update(team_id, sheet_name, num_rows_updated):
+    # Carregar configurações das equipes
+    teams_config = load_json_file('teams_config.json')
+
+    team_config = teams_config.get(team_id)
+    if not team_config:
+        logger.error(f"Configuração não encontrada para o time: {team_id}")
+        return
+
+    # Mensagem para o Slack
+    message = f"Foram alteradas {num_rows_updated} linhas na planilha '{
+        sheet_name}' esta semana."
+    send_slack_message(slack_client, team_config['channel'], message)
