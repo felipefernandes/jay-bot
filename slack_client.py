a1568eb4be1ca682f3123eb42baa8f27de1053a7
Felipe Fernandes <felipefernandesweb@gmail.com>
2024-12-10 17:41:39 -0300

    melhoria na formatacao da mensagem slack com formatacao em Blocks

--- slack_client.py
+++ slack_client.py
@@ -21,13 +21,36 @@ class SlackClient:
 
         self.client = WebClient(token=token)
 
-    def send_message(self, channel, message, debug=0):
-        try:
-            if debug == 1:
-                message = "⚠️ *Modo DEBUG Ativado*\n" + message
+    def send_message(self, channel, message=None, blocks=None, debug=0):
+        """
+        Envia uma mensagem para um canal específico no Slack.
 
+        :param channel: O canal para enviar a mensagem.
+        :param message: Texto simples da mensagem.
+        :param blocks: Blocos de mensagem em formato JSON.
+        :param debug: Flag para habilitar o modo debug (0 ou 1).
+        """
+        try:
+            # Adiciona modo debug à mensagem ou aos blocos
+            if debug == 1:
+                debug_message = "⚠️ *Modo DEBUG Ativado*\n"
+                if blocks:
+                    blocks.insert(0, {
+                        "type": "section",
+                        "text": {
+                            "type": "mrkdwn",
+                            "text": debug_message
+                        }
+                    })
+                elif message:
+                    message = debug_message + message
+
+            # Envia a mensagem, priorizando blocos
             response = self.client.chat_postMessage(
-                channel=channel, text=message)
+                channel=channel,
+                text=message if not blocks else None,
+                blocks=blocks
+            )
 
             if not response.get("ok"):
                 print(f"Erro ao enviar mensagem: {response.get('error')}")
